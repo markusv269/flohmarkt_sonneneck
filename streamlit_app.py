7099872b2be5a74dfb064e11aabb6df8bc58a27f
markusv269 <60115561+markusv269@users.noreply.github.com>
2025-11-16 15:12:06 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Flohmarkt Sonneneck Eilenburg")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Willkommen auf unserem Online Flohmarkt."
 )
+
+
